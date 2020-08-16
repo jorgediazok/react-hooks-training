e41b1f58c904bb296859daed77270153f6b930a8
Jorge <jorgediazok@gmail.com>
2020-08-16 20:41:07 -0300

    methods

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>We are going to train a bit with React</h1>
-      <p>PATADA DE CANGURO GOLPE DURO</p>
-      <Person name="Jorge" age="36" />
-      <Person name="Wos" age="25">
-        Hobbies: Singing
-      </Person>
-      <Person name="Rupert" age="29" />
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Jorge', age: 36 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 },
+    ],
+  };
+
+  switchNameHandler = () => {
+    console.log('WHATDAFUCK');
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>We are going to train a bit with React</h1>
+        <p>PATADA DE CANGURO GOLPE DURO</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
+          Hobbies: Singing
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
