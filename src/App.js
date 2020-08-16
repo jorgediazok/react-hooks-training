5f7a5ddfb5e557898722d6bb4e3ae2f881980275
Jorge <jorgediazok@gmail.com>
2020-08-15 23:43:35 -0300

    functional component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Person from './Person/Person';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>We are going to train a bit with React</h1>
+      <p>PATADA DE CANGURO GOLPE DURO</p>
+      <Person />
     </div>
   );
 }
