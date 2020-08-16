71d122ef28edf485af58b046779a22a84fd951b2
Jorge <jorgediazok@gmail.com>
2020-08-16 01:12:27 -0300

    props children

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ function App() {
     <div className="App">
       <h1>We are going to train a bit with React</h1>
       <p>PATADA DE CANGURO GOLPE DURO</p>
-      <Person />
+      <Person name="Jorge" age="36" />
+      <Person name="Wos" age="25">
+        Hobbies: Singing
+      </Person>
+      <Person name="Rupert" age="29" />
     </div>
   );
 }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
-const person = () => {
-  return <p>I am a person!</p>;
+const person = (props) => {
+  return (
+    <div>
+      <p>
+        I am a {props.name} and i am {props.age}
+      </p>
+      <p>{props.children}</p>
+    </div>
+  );
 };
 
 export default person;
